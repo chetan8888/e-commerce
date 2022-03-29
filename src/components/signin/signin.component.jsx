63049d943897b8dcab48d0fe5ec2de88ad271d21
CHETAN GOUR <chetangour88@gmail.com>
2022-03-28 18:14:54 -0700

    Google sign in with redirect updated

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -1,17 +1,27 @@
 import { Fragment } from "react";
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+
+    // When we signin using redirect, we will move to a new url and once the authentication is successful there we will be redirected back to our current page. Now the auth object will store the authentication details and since the component is remounted the useEffect will run and this is how we can get the authentication details.
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+    //     if (response) {
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //     }
+    // }, [])
+
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
 
-
     return (
         <Fragment>
             <h2>Sign In Page</h2>
             <button onClick={logGoogleUser}> Sign In with Google Popup</button>
+            {/* <button onClick={signInWithGoogleRedirect}> Sign In with Google redirect</button> */}
         </Fragment>
     )
 }
